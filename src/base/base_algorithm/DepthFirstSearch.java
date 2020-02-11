205d021fded5fae8633fe46beac0df5396c50b73
Genius_DSY <genius_dsy@foxmail.com>
2020-02-11 21:40:57 +0800

    :fire: 搜索——深度优先搜索

---
+++ src/base/base_algorithm/DepthFirstSearch.java
@@ -0,0 +1,31 @@
+package base.base_algorithm;
+
+import base.base_structure.TreeNode;
+import base.base_structure.GraphNode;
+import java.util.Stack;
+
+/**
+ * @author :DengSiYuan
+ * @date :2020/2/11 21:14
+ * @desc : 深度优先搜索
+ */
+public class DepthFirstSearch {
+
+    public void depthFirstSearchTree(TreeNode nodeHead) {
+        if(nodeHead == null) {
+            return;
+        }
+        Stack<TreeNode> myStack = new Stack<>();
+        myStack.add(nodeHead);
+        while(!myStack.isEmpty()) {
+            TreeNode node = myStack.pop();    //弹出栈顶元素
+            System.out.print(node.val+" ");
+            if(node.right != null) {
+                myStack.push(node.right);    //深度优先遍历，先遍历左边，后遍历右边,栈先进后出
+            }
+            if(node.left != null) {
+                myStack.push(node.left);
+            }
+        }
+    }
+}
