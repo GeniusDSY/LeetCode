39c066d37baff856c48d6c60e24affc60394dcc5
Genius_DSY <genius_dsy@foxmail.com>
2020-02-12 17:39:06 +0800

    :fire: 基础结构——双链表

---
+++ src/base/base_structure/DoubleListNode.java
@@ -0,0 +1,16 @@
+package base.base_structure;
+
+/**
+ * @author :DengSiYuan
+ * @date :2020/2/12 11:01
+ * @desc : 基础结构——双链表
+ */
+public class DoubleListNode {
+
+    public int val;
+    public DoubleListNode prev,next;
+    public DoubleListNode(int x) {
+        val = x;
+    }
+
+}
